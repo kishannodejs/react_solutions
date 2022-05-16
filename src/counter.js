40def5701156aec189bbeb34065f894b9f59a52e
Kishan Prajapati <kishan.nodejs@gmail.com>
2022-05-16 19:45:09 +0530

    React Solutions

---
+++ src/counter.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+export default function App() {
+const [count, setCount] = useState(0);
+const increment = () =>{
+   setCount(count+1);
+}
+const decrement = () =>{
+   setCount(count-1);
+}
+const reset = () =>{
+   setCount(0);
+}
+return (
+<div className="App">
+<button className='inc' onClick={increment}>Increment!</button>
+<button className='dec' onClick={decrement}>Decrement!</button>
+<button className='reset' onClick={reset}>Reset</button>
+<h1>Current Count: {count}</h1>
+</div>
+);
+}
